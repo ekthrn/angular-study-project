04a9f9491d5977677079b8b9184a1cc0606149ec
ekthrn <myektrn@gmail.com>
2026-02-26 00:17:58 +0300

    add sorting by options in MenuFilterService

--- src/app/shared/services/menu-filter.service.ts
+++ src/app/shared/services/menu-filter.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 
+import { OptionSort } from '@models/option.model';
+
 @Injectable({
   providedIn: 'root',
 })
 export class MenuFilterService {
-  private selectedSource = new BehaviorSubject<string | null>(null);
-  selectedItem$: Observable<string | null> = this.selectedSource.asObservable();
+  private menuSource = new BehaviorSubject<string | null>(null);
+  menuItem$: Observable<string | null> = this.menuSource.asObservable();
 
-  public setItem(item: string) {
-    this.selectedSource.next(item);
+  private sortSource = new BehaviorSubject<OptionSort | null>(null);
+  sortOption$ = this.sortSource.asObservable();
+
+  public setMenuItem(item: string) {
+    this.menuSource.next(item);
+  }
+  public setSortOption(item: OptionSort) {
+    this.sortSource.next(item);
   }
 }
