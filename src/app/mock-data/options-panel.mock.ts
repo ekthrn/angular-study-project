a5cdbd910507b84b1da854aedb4cf45f2bb807ab
ekthrn <myektrn@gmail.com>
2026-03-07 18:16:31 +0300

    put the data of the options for sorting books in a separate mock file

---
+++ src/app/mock-data/options-panel.mock.ts
@@ -0,0 +1,6 @@
+import {OptionSort} from "@models/option.model";
+
+export const MOCK_PANEL_OPTIONS: OptionSort[] = [
+  { id: 'name', label: 'Наименованию', direction: 'asc'},
+  // { id: 'rating', label: 'Рейтингу', direction: 'asc'}
+];
