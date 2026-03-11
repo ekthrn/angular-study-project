df132d6afce985857b5a513ce7ab90efb58c1d8b
ekthrn <myektrn@gmail.com>
2026-03-11 23:37:30 +0300

    add parameters for searching in FilterState

--- src/app/shared/models/filter-state.model.ts
+++
@@ -1,8 +0,0 @@
-import {OptionSort, Book} from '@models';
-
-export type FilterState = {
-  optionMenu: string | null,
-  optionSort: OptionSort | null,
-  featuredItems: Book[] | []
-  curHoverBook: Book | null
-}
