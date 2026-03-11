5a9c444958d6be394fdb09c74dc1df18a72b8d2f
ekthrn <myektrn@gmail.com>
2026-03-11 23:34:30 +0300

    create mock data on search options

--- src/app/mock-data/options-panel.mock.ts
+++
@@ -1,6 +0,0 @@
-import {OptionSort} from "@models/option.model";
-
-export const MOCK_PANEL_OPTIONS: OptionSort[] = [
-  { id: 'name', label: 'Наименованию', direction: 'asc'},
-  // { id: 'rating', label: 'Рейтингу', direction: 'asc'}
-];
