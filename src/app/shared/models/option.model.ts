613752e486e44506f5484101d52da49c57fb417a
ekthrn <myektrn@gmail.com>
2026-03-11 23:36:52 +0300

    create new type OptionSearch

--- src/app/shared/models/option.model.ts
+++ src/app/shared/models/option.model.ts
@@ -1,8 +1,19 @@
 type Direction = 'asc' | 'desc';
+
+export type FilterOption = OptionSort | OptionSearch;
+
 export type OptionSort = {
+  type: string,
   id: string,
   label: string,
   direction: Direction,
+  isSelected?: boolean,
 };
 
-
+export type OptionSearch = {
+  type: string,
+  id: string,
+  label: string,
+  isSelected?: boolean,
+  query?: string,
+}
